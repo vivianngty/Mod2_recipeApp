7bfa31c74d7a927f6017809a85b0640b69815d1a
Vivian Ng <vivian.ng.t.y@gmail.com>
2021-01-19 22:46:50 -0500

    second commit

---
+++ recipe_app/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div className="header">
+                <h1>Welcome!</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
